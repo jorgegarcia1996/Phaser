2500d9f82c3ac38e9525d252c340f3e6187d832d
Jorge García Molina <43571181+jorgegarcia1996@users.noreply.github.com>
2019-10-24 20:25:49 +0200

    Label puesto pero no funciona

--- Score-Live-Menu-GameOver/Scene2.js
+++ Score-Live-Menu-GameOver/Scene2.js
@@ -8,9 +8,16 @@ class Scene2 extends Phaser.Scene {
     this.background = this.add.tileSprite(0, 0, config.width, config.height, "background");
     this.background.setOrigin(0, 0);
 
+    this.enemies = this.physics.add.group();
+
+
     this.enemy1 = this.physics.add.sprite(config.width, config.height + 50, "enemy1");
     this.enemy2 = this.physics.add.sprite(config.width, config.height / 2, "enemy2");
     this.enemy3 = this.physics.add.sprite(config.width, config.height / 2 - 50, "enemy3");
+
+    this.enemies.add(this.enemy1);
+    this.enemies.add(this.enemy2);
+    this.enemies.add(this.enemy3);
 
     this.enemy1.play("enemy1_anim");
     this.enemy2.play("enemy2_anim");
@@ -22,11 +29,6 @@ class Scene2 extends Phaser.Scene {
 
     this.input.on('gameobjectdown', this.destroy, this);
 
-    this.add.text(20, 20, "Playing game", {
-      font: "25px Arial",
-      fill: "yellow"
-    });
-
     this.physics.world.setBoundsCollision();
 
     this.items = this.physics.add.group();
@@ -34,7 +36,7 @@ class Scene2 extends Phaser.Scene {
     for (var i = 0; i < gameSettings.maxPowerups; i++) {
       var item = this.physics.add.sprite(32, 30, "items");
       this.items.add(item);
-       item.setRandomPosition(0, 0, game.config.width, game.config.height);
+      item.setRandomPosition(0, 0, game.config.width, game.config.height);
 
       if (Math.random() > 0.5) {
         item.play("note");
@@ -51,6 +53,7 @@ class Scene2 extends Phaser.Scene {
     this.m = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.M);
 
     this.projectiles = this.add.group();
+    this.players = this.add.group();
 
     this.player1 = this.physics.add.sprite(105, config.height / 2 - 80, "player1");
     this.player1.play("player1_anim");
@@ -62,9 +65,67 @@ class Scene2 extends Phaser.Scene {
     this.cursorKeys = this.input.keyboard.createCursorKeys();
     this.player2.setCollideWorldBounds(true);
 
-    this.physics.add.collider(this.player1, this.player2);
+    this.players.add(this.player1);
+    this.players.add(this.player2);
+
+    this.physics.add.collider(this.players, this.players);
     this.physics.add.collider(this.items, this.items);
 
+    this.physics.add.overlap(this.players, this.items, this.pickItem, null, this);
+    this.physics.add.overlap(this.projectiles, this.enemies, this.hitEnemy, null, this);
+
+
+    var graphics = this.add.graphics();
+    graphics.fillStyle(0x000000, 1);
+    graphics.beginPath();
+    graphics.moveTo(0, 0);
+    graphics.lineTo(config.width, 0);
+    graphics.lineTo(config.width, 80);
+    graphics.lineTo(0, 80);
+    graphics.lineTo(0, 0);
+    graphics.closePath();
+    graphics.fillPath();
+
+    this.scoreP1 = 0;
+    this.scoreP2 = 0;
+    this.livesP1 = 3;
+    this.livesP2 = 3;
+
+    var scoreP1Formated = this.zeroPad(this.scoreP1, 6);
+    var scoreP2Formated = this.zeroPad(this.scoreP1, 6);
+
+    this.scoreLabelP1 = this.add.bitmapText(10, 5, "pixelFont", "P1 -  SCORE " + scoreP1Formated, 36);
+    this.scoreLabelP2 = this.add.bitmapText(10, 50, "pixelFont", "P2 - SCORE " + scoreP2Formated, 36);
+    this.livesLabelP1 = this.add.bitmapText(700, 5, "pixelFont", "LIVES " + this.livesP1, 36);
+    this.livesLabelP2 = this.add.bitmapText(700, 50, "pixelFont", "LIVES " + this.livesP2, 36);
+
+
+  }
+
+  zeroPad(number, size) {
+    var stringNumber = String(number);
+    while (stringNumber.length < (size || 2)) {
+      stringNumber = "0" + stringNumber;
+    }
+    return stringNumber;
+  }
+
+  pickItem(player, powerUp) {
+    powerUp.disableBody(true, true);
+  }
+
+  hitEnemy(player, projectile, enemy) {
+    projectile.destroy();
+    this.resetPos(enemy);
+    if(player == this.player1) {
+      this.scoreP1 += 15;
+     var scoreP1Formated = this.zeroPad(this.scoreP1 , 6);
+     this.scoreLabelP1.text = "P1 -  SCORE " + scoreP1Formated;
+    } else {
+      this.scoreP2 += 15;
+     var scoreP2Formated = this.zeroPad(this.scoreP2, 6);
+     this.scoreLabelP2.text = "P2 - SCORE " + scoreP2Formated;
+    }
   }
 
   update() {
@@ -109,47 +170,47 @@ class Scene2 extends Phaser.Scene {
   }
 
   shoot(player) {
-    var bullet = new Bullet(this,player);
+    var bullet = new Bullet(this, player);
   }
 
-  movePlayer1Manager(){
+  movePlayer1Manager() {
 
     this.player1.setVelocity(0);
 
-    if(this.cursorKeys.left.isDown){
+    if (this.cursorKeys.left.isDown) {
       this.player1.setVelocityX(-gameSettings.playerSpeed);
-    }else if(this.cursorKeys.right.isDown){
+    } else if (this.cursorKeys.right.isDown) {
       this.player1.setVelocityX(gameSettings.playerSpeed);
     }
 
-    if(this.cursorKeys.up.isDown){
+    if (this.cursorKeys.up.isDown) {
       this.player1.setVelocityY(-gameSettings.playerSpeed);
-    }else if(this.cursorKeys.down.isDown){
+    } else if (this.cursorKeys.down.isDown) {
       this.player1.setVelocityY(gameSettings.playerSpeed);
     }
   }
 
-  movePlayer2Manager(){
+  movePlayer2Manager() {
 
     this.player2.setVelocity(0);
 
     this.wasdKeys = this.input.keyboard.addKeys({
-      wKey:Phaser.Input.Keyboard.KeyCodes.W,
-      sKey:Phaser.Input.Keyboard.KeyCodes.S,
-      aKey:Phaser.Input.Keyboard.KeyCodes.A,
-      dKey:Phaser.Input.Keyboard.KeyCodes.D,
-      
-      });
+      wKey: Phaser.Input.Keyboard.KeyCodes.W,
+      sKey: Phaser.Input.Keyboard.KeyCodes.S,
+      aKey: Phaser.Input.Keyboard.KeyCodes.A,
+      dKey: Phaser.Input.Keyboard.KeyCodes.D,
 
-    if(this.wasdKeys.aKey.isDown){
+    });
+
+    if (this.wasdKeys.aKey.isDown) {
       this.player2.setVelocityX(-gameSettings.playerSpeed);
-    }else if(this.wasdKeys.dKey.isDown){
+    } else if (this.wasdKeys.dKey.isDown) {
       this.player2.setVelocityX(gameSettings.playerSpeed);
     }
 
-    if(this.wasdKeys.wKey.isDown){
+    if (this.wasdKeys.wKey.isDown) {
       this.player2.setVelocityY(-gameSettings.playerSpeed);
-    }else if(this.wasdKeys.sKey.isDown){
+    } else if (this.wasdKeys.sKey.isDown) {
       this.player2.setVelocityY(gameSettings.playerSpeed);
     }
   }
@@ -161,13 +222,12 @@ class Scene2 extends Phaser.Scene {
     }
   }
 
-  resetPos(ship){
-    ship.x = config.width;
+  resetPos(enemy) {
+    enemy.x = config.width;
     var randomY = Phaser.Math.Between(config.top_height, config.bottom_height);
-    ship.y = randomY;
+    enemy.y = randomY;
   }
 
-  // 1.3
   destroy(pointer, gameObject) {
     gameObject.setTexture("explosion");
     gameObject.play("explode");
