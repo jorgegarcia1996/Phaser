982c567406354e416aa65b63f824b21f01664416
Jorge García Molina <43571181+jorgegarcia1996@users.noreply.github.com>
2019-11-03 20:40:08 +0100

    El juego comienza al clicar en 1 jugador

--- PipoGame/scene02_main_menu.js
+++ PipoGame/scene02_main_menu.js
@@ -7,14 +7,25 @@ class scene02_main_menu extends Phaser.Scene {
         this.background = this.add.tileSprite(0, 0, config.width, config.height, "background");
         this.background.setOrigin(0, 0);
 
-        this.logo = this.add.tileSprite(config.width/2, 150, 400, 200, "Logo");
-        this.onePlayerLogo = this.add.tileSprite(config.width/2 - 200, 370, 200, 200, "Pipo");
-        this.TwoPlayerLogo = this.add.tileSprite(config.width/2 + 200, 370, 250, 200, "Pipos");
+        this.logo = this.add.tileSprite(config.width/2, 120, 400, 200, "Logo");
+        this.onePlayerLogo = this.physics.add.sprite(config.width/2 - 200, 370, "Pipo");
+        this.twoPlayerLogo = this.physics.add.sprite(config.width/2 + 200, 370, "Pipos");
+        
+        this.onePlayerLogo.setInteractive();
+        this.twoPlayerLogo.setInteractive();
+
+        this.input.on('gameobjectdown', this.startGame, this);
 
     }
 
     update() {
         this.background.tilePositionX += 1;
         this.background.alpha = 0.3;
+    }
+
+    startGame(pointer, gameObject) {
+        if (gameObject == this.onePlayerLogo) {
+            this.scene.start("game");
+        }
     }
 }
--- PipoGame/scene03_game.js
+++ PipoGame/scene03_game.js
@@ -2,4 +2,14 @@ class scene03_game extends Phaser.Scene {
     constructor() {
         super("game");
     }
+
+    create() {
+
+        this.background = this.add.tileSprite(0, 0, config.width, config.height, "background");
+        this.background.setOrigin(0, 0);
+    }
+
+    update() {
+        this.background.tilePositionX += 1;
+    }
 }
